697106bc0272d35d5ae2ff1f78b586fe94311268
auzpa <auzpa@LAPTOP-DR3V7M0S>
2020-07-11 00:38:46 +0800

    added leg mode change and reroute module as part of MaaS strategy

--- maas/src/main/java/singlePlanAlgo/MaaSPlanStrategy.java
+++ maas/src/main/java/singlePlanAlgo/MaaSPlanStrategy.java
@@ -1,6 +1,7 @@
 package singlePlanAlgo;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.HasPlansAndId;
@@ -8,10 +9,17 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.ChangeModeConfigGroup;
+import org.matsim.core.config.groups.GlobalConfigGroup;
 import org.matsim.core.replanning.PlanStrategy;
 import org.matsim.core.replanning.PlanStrategyImpl;
 import org.matsim.core.replanning.ReplanningContext;
+import org.matsim.core.replanning.modules.ChangeLegMode;
+import org.matsim.core.replanning.modules.ReRoute;
+import org.matsim.core.replanning.modules.TripsToLegsModule;
 import org.matsim.core.replanning.selectors.ExpBetaPlanSelector;
+import org.matsim.core.router.TripRouter;
+import org.matsim.facilities.ActivityFacilities;
 
 import com.google.inject.name.Named;
 
@@ -24,13 +32,14 @@ public class MaaSPlanStrategy implements PlanStrategy{
 	
 	@Inject
 	private @Named(MaaSUtil.MaaSPackagesAttributeName) MaaSPackages packages;
-
+	
 	@Inject // The constructor must be annotated so that the framework knows which one to use.
-	MaaSPlanStrategy(Config config,Scenario scenario, EventsManager eventsManager,@Named(MaaSUtil.MaaSPackagesAttributeName) MaaSPackages packages) {
+	MaaSPlanStrategy(Config config,Scenario scenario, EventsManager eventsManager,@Named(MaaSUtil.MaaSPackagesAttributeName) MaaSPackages packages,
+			GlobalConfigGroup globalConfigGroup, ChangeModeConfigGroup changeLegModeConfigGroup, ActivityFacilities activityFacilities, Provider<TripRouter> tripRouterProvider) {
 		// A PlanStrategy is something that can be applied to a Person (not a Plan).
         // It first selects one of the plans:
         //MyPlanSelector planSelector = new MyPlanSelector();
-        
+		
 
         // the plan selector may, at the same time, collect events:
         //eventsManager.addHandler(planSelector);
@@ -44,7 +53,10 @@ public class MaaSPlanStrategy implements PlanStrategy{
 		MaaSStrategyModule mod = new MaaSStrategyModule(packages);
 		PlanStrategyImpl.Builder builder = new PlanStrategyImpl.Builder(new ExpBetaPlanSelector(config.planCalcScore()));
         builder.addStrategyModule(mod);
-
+        builder.addStrategyModule(new TripsToLegsModule(tripRouterProvider, globalConfigGroup));
+		builder.addStrategyModule(new ChangeLegMode(globalConfigGroup, changeLegModeConfigGroup));
+		builder.addStrategyModule(new ReRoute(activityFacilities, tripRouterProvider, globalConfigGroup));
+         
         // these modules may, at the same time, be events listeners (so that they can collect information):
         eventsManager.addHandler(mod);
 
